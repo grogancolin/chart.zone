eabc628c35464b66829c6e91b49426c9590c194a
Colin Grogan <grogan.colin@gmail.com>
2014-08-27 00:22:29 +0100

    Added new charts

--- source/chartzone/datafetchers.d
+++ source/chartzone/datafetchers.d
@@ -70,7 +70,6 @@ public class ChartFetcherException : Exception{
 	Parses it into the ChartEntry struct and returns that.
 */
 public ChartEntry getChart_BBCTop40(){
-	logInfo("In datafetchers.d getBBCTOP40");
 	// read from BBC
 	string url = "http://www.bbc.co.uk/radio1/chart/singles";
 	string bbcTop40 = getDataFromURL(url);
@@ -104,7 +103,7 @@ public ChartEntry getChart_BBCTop40(){
 			ele[0].innerHTML.htmlEntitiesDecode,
 			"unknown-id",
 			i++,
-			["BBCTop40", "pop"],
+			["BBCTop40", "Pop"],
 			"unknown-url"
 			);
 	}
@@ -115,15 +114,59 @@ public ChartEntry getChart_BBCTop40(){
 
 }
 
+public ChartEntry getChart_BBCTop40Indie(){
+	// read from BBC
+	string url = "http://www.bbc.co.uk/radio1/chart/indiesingles";
+	string bbcTop40 = getDataFromURL(url);
+	
+	Document htmlObj = new Document();
+	htmlObj.parse(bbcTop40);
+	auto artistListing = htmlObj.getElementsBySelector(`div[class="cht-entry-artist"]`);
+	auto trackListing = htmlObj.getElementsBySelector(`div[class="cht-entry-title"]`);
+	
+	// ensure we have equal numbers
+	if(artistListing.length == 0 || trackListing.length == 0){
+		logInfo("Invalid info from %s", url);
+		throw new ChartFetcherException("Received no 'div[class=\"cht-entry-artist\"]' tags from " ~ url);
+	}
+	if(artistListing.length != trackListing.length){
+		logInfo("Invalid info from %s", url);
+		throw new ChartFetcherException("Error parsing information from " ~ url);
+	}
+	
+	SongEntry[] songs;
+	string nameToSearch;
+	string videoId;
+	auto artist_track = zip(artistListing, trackListing);
+	uint i=1;
+	foreach(ele; artist_track){
+		
+		//Add song to playlist
+		//addVideoToPlaylist(playListId, videoId);
+		songs ~= SongEntry(
+		ele[1].innerHTML.htmlEntitiesDecode,
+		ele[0].innerHTML.htmlEntitiesDecode,
+		"unknown-id",
+		i++,
+		["BBCTop40Indie", "Indie"],
+		"unknown-url"
+		);
+	}
+	
+	return ChartEntry(
+		"BBCTop40Indie", "uk", "none",  songs
+		);
+	
+}
+
 /**
 	Goes to the BBC radio1 dance chart and gets top 40.
 	Parses it into the ChartEntry struct and returns that.
 */
 public ChartEntry getChart_BBCTop40Dance(){
+	string url = "http://www.bbc.co.uk/radio1/chart/dancesingles";
 	// read from BBC
-	string bbcTop40 = getDataFromURL("http://www.bbc.co.uk/radio1/chart/dancesingles");
-
-
+	string bbcTop40 = getDataFromURL(url);
 
 	Document htmlObj = new Document();
 	htmlObj.parse(bbcTop40);
@@ -132,7 +175,7 @@ public ChartEntry getChart_BBCTop40Dance(){
 
 	// ensure we have equal numbers
 	if(artistListing.length == 0 || trackListing.length == 0){
-		throw new ChartFetcherException("Received no 'div[class=\"cht-entry-artist\"]' tags from http://www.bbc.co.uk/radio1/chart/dancesingles");
+		throw new ChartFetcherException("Received no 'div[class=\"cht-entry-artist\"]' tags from "~ url);
 	}
 	if(artistListing.length != trackListing.length){
 		throw new ChartFetcherException("Error parsing information from http://www.bbc.co.uk/radio1/chart/dancesingles");
@@ -141,8 +184,6 @@ public ChartEntry getChart_BBCTop40Dance(){
 
 
 	SongEntry[] songs;
-	string nameToSearch;
-	string videoId;
 
 	auto artist_track = zip(artistListing, trackListing);
 	uint i=1;
@@ -162,6 +203,95 @@ public ChartEntry getChart_BBCTop40Dance(){
 		);
 }
 
+public ChartEntry getChart_BBCTop40Rock(){
+	string url = "http://www.bbc.co.uk/radio1/chart/rocksingles";
+	// read from BBC
+	string bbcTop40 = getDataFromURL(url);
+
+	Document htmlObj = new Document();
+	htmlObj.parse(bbcTop40);
+	auto artistListing = htmlObj.getElementsBySelector(`div[class="cht-entry-artist"]`);
+	auto trackListing = htmlObj.getElementsBySelector(`div[class="cht-entry-title"]`);
+	
+	// ensure we have equal numbers
+	if(artistListing.length == 0 || trackListing.length == 0){
+		throw new ChartFetcherException("Received no 'div[class=\"cht-entry-artist\"]' tags from " ~ url);
+	}
+	if(artistListing.length != trackListing.length){
+		throw new ChartFetcherException("Error parsing information from " ~ url);
+	}
+
+	SongEntry[] songs;
+
+	auto artist_track = zip(artistListing, trackListing);
+	uint i=1;
+	foreach(ele; artist_track){
+		
+		songs ~= SongEntry(
+			ele[1].innerHTML.htmlEntitiesDecode,
+		ele[0].innerHTML.htmlEntitiesDecode,
+		"unknown-id",
+		i++,
+		["BBCTop40Rock", "Rock"],
+		"unknown-url"
+		);
+	}
+	return ChartEntry(
+		"BBCTop40Rock", "uk", "none",  songs
+		);
+}
+
+
+
+public ChartEntry getChart_BillboardTop25Rock(){
+	string[] urls;
+	urls ~= "http://www.billboard.com/charts/rock-songs";
+	foreach(i; 1..3){ // create the URLs we're going to use
+		urls ~= format("%s?page=%s", urls[0], i);
+	}
+
+	SongEntry[] songs;
+
+	foreach(url; urls){
+
+		string billboardStr = getDataFromURL(url);
+
+		Document htmlObj = new Document();
+		htmlObj.parse(billboardStr);
+		auto chartListing = htmlObj.getElementsBySelector(`div[class="listing chart_listing"]`);
+		if(chartListing.length != 1)
+			throw new ChartFetcherException("Error parsing response from BillboardTop25Rock. Error: couldnt find <div class=\"listing chart_listing\">");
+		
+		foreach(songEntry; chartListing[0].getElementsByTagName(`article`)){
+			string position = songEntry
+				.getElementsByTagName(`a`)[0] // the first <a is the position
+			.getAttribute("id")
+				.strip()	//remove any pre and post whitespace
+					.replace("rank_", "");
+			
+			string artist = songEntry
+				.getElementsByTagName(`a`)[1] // the second <a> is the artist
+			.getAttribute("title")
+				.strip();
+			
+			string songTitle = songEntry
+				.getElementsByTagName(`h1`)[0] // the first h1 is the song title
+			.innerHTML()
+				.chomp();
+			
+			// append a new song object to songs[]
+			songs ~= SongEntry(
+				songTitle.htmlEntitiesDecode,
+				artist.htmlEntitiesDecode,
+				"unknown-id",
+				position.to!uint,
+				["BillboardTop25Rock", "Rock"],
+				"unknown-url"
+			);
+		}
+	}
+	return ChartEntry("BillboardTop25Rock", "usa", "none",  songs);
+}
 
 /**
 	Gets the top 100 off billboard hot 100, and parses it to generate a ChartEntry object.
@@ -187,12 +317,7 @@ public ChartEntry getChart_BillboardTop100(){
 		urls ~= format("%s?page=%s", urls[0], i);
 	}
 
-	//Create Playlist and get playlist Id
-	string playListId = "temp"; //createPlaylist("Billboard Top 100".getChartTitleDate());
-
 	SongEntry[] songs;
-	string nameToSearch;
-	string videoId;
 
 	foreach(url; urls){
 		string billboardStr = getDataFromURL(url);
@@ -200,8 +325,8 @@ public ChartEntry getChart_BillboardTop100(){
 		Document htmlObj = new Document();
 		htmlObj.parse(billboardStr);
 		auto chartListing = htmlObj.getElementsBySelector(`div[class="listing chart_listing"]`);
-		assert(chartListing.length==1,
-			"Error parsing response from Billboard 100. Error: couldnt find <div class=\"listing chart_listing\">");
+		if(chartListing.length != 1)
+			throw new ChartFetcherException("Error parsing response from BillboardTop100. Error: couldnt find <div class=\"listing chart_listing\">");
 
 		foreach(songEntry; chartListing[0].getElementsByTagName(`article`)){
 			string position = songEntry
@@ -226,7 +351,7 @@ public ChartEntry getChart_BillboardTop100(){
 					artist.htmlEntitiesDecode,
 					"unknown-id",
 					position.to!uint,
-				["BillboardTop100", "pop"],
+				["BillboardTop100", "Pop"],
 				"unknown-url"
 				);
 		}
@@ -251,8 +376,8 @@ public ChartEntry getChart_ItunesTop100(){
     Document htmlObj = new Document();
 	htmlObj.parse(iTunesTop100);
 	auto chartListing = htmlObj.getElementsByTagName(`ul`);
-	assert(chartListing.length >= 1,
-			"Error parsing response from iTunes Top 100. Error: couldnt find at least one ul tag");
+	if(chartListing.length == 0)
+		throw new ChartFetcherException("Error parsing response from iTunesTop100. Error: couldnt find at least one ul tag");
 
 	SongEntry[] songs;
 	string nameToSearch;
@@ -270,7 +395,7 @@ public ChartEntry getChart_ItunesTop100(){
 					artist.htmlEntitiesDecode,
 					"unknown-id",
 					i++,
-					["ItunesTop100", "pop"],
+					["ItunesTop100", "Pop"],
 					"unknown-url"
 				);
          }
